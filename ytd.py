bf859dddc9c6892ae0870031ca9ae6c416b78f7a
nR3D <alberto.blaster.jr+github@gmail.com>
2018-10-15 08:57:52 +0200

    added playlist items, start and end args

--- ytd.py
+++ ytd.py
@@ -38,6 +38,13 @@ def check_args(args=None, default=None):
                         help="Directly download video, without further CLI interactions")
     parser.add_argument('--playlist', '-p',
                         help="Donwload an entire playlist")
+    parser.add_argument('--playlist-items', '-pi',
+                        help="Select items on a list to download (index starts at 1), e.g. -pi '1,2,4'")
+    parser.add_argument('--playlist-start', '-ps',
+                        default=1,
+                        help="Select strating item on a playlist (index starts at 1)")
+    parser.add_argument('--playlist-end', '-pe',
+                        help="Select ending itam on a playlist (index starts at 1)")
     return parser.parse_args(args)
 
 
@@ -60,12 +67,16 @@ def exit_message(t):
     print("\n %s Has been downloaded" % t)
 
 
-def download(song=None, folder_path=None, playlist=False):
+def download(song=None, folder_path=None, playlist=False, playlist_items=None, playlist_start=1, playlist_end=None):
 
     ytdl_options = {
         'format': 'bestaudio/best', # select quality
         'outtmpl': "{}/%(title)s.%(ext)s".format(os.path.normpath(folder_path)),
+        'no_warnings': True,
         'noplaylist': not playlist,
+        'playlist_items': playlist_items, # str type if used (e.g '1,2,4'), otherwise None value
+        'playliststart': playlist_start, # int type if used, otherwise 1 to start from the beginning
+        'playlistend': playlist_end, # int type if used, otherwise None value
         'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
@@ -113,7 +124,39 @@ def main():
         if check_args(sys.argv[1:]).video:
             download(song=check_args(sys.argv[1:]).video, folder_path=path)
         elif check_args(sys.argv[1:]).playlist:
-            download(song=check_args(sys.argv[1:]).playlist, folder_path=path, playlist=True)
+
+            #check if --playlist_items format is correct
+            if check_args(sys.argv[1:]).playlist_items:
+                try:
+                    # the following assignment is only to check the format, after that we can turn back to a str value.
+                    playlist_items = [int(item) for item in check_args(sys.argv[1:]).playlist_items.split(',')]
+                    playlist_items = check_args(sys.argv[1:]).playlist_items
+                except ValueError:
+                    print("ValueError: --playlist-items must be integers and divided by commas, e.g. -pi '1,2,4'")
+            else:
+                playlist_items = None
+            
+            # check if --playlist-start can be converted into an integer, otherwise 1 is assigned
+            if check_args(sys.argv[1:]).playlist_start:
+                try:
+                    playlist_start = int(check_args(sys.argv[1:]).playlist_start)
+                except ValueError:
+                    print('ValueError: --playlist-start must an integer')
+            else:
+                playlist_start = 1
+
+            # check if --playlist-start can be converted into an integer, otherwise None value is assigned
+            if check_args(sys.argv[1:]).playlist_end:
+                try:
+                    playlist_end = int(check_args(sys.argv[1:]).playlist_end)
+                except ValueError:
+                    print('ValueError: --playlist-end must be an integer')
+            else:
+                playlist_end = None
+
+            print(playlist_items, playlist_start, playlist_end, sep='----\n', end='----\n')
+            download(song=check_args(sys.argv[1:]).playlist, folder_path=path, playlist=True,
+                    playlist_items=playlist_items, playlist_start=playlist_start, playlist_end=playlist_end)
         else:
             while True:
                 download(folder_path=path)
